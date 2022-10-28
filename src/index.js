b6b7c11e263f81ac68daf2b47c72fdab9a4bd0af
Dev_HoaLe <leconghoa11a1@gmail.com>
2022-10-28 11:34:56 +0700

    Prettier - Code formatter

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ app.set("views", path.join(__dirname, "resources/views"));
 // Home, search, contact
 
 // Route init
-route(app);
+    route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
