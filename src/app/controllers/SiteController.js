02aa05fca0bdc16f7cee9786fce2aebb229f7a68
Dev_HoaLe <leconghoa11a1@gmail.com>
2022-10-28 17:14:44 +0700

    read data from DB

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,14 +1,13 @@
-const course = require("../models/Course");
+const Course = require("../models/Course");
+const { multipleMongooseToObject } = require("../../util/mongoose");
 class SiteController {
   // [GET] /home
-  index(req, res) {
-    res.render("home");
-    course.find({}, function (err, courses) {
-      if (!err) res.json(courses);
-      else {
-        res.status(400).json();
-      }
-    });
+  index(req, res, next) {
+    Course.find({})
+      .then((courses) =>
+        res.render("home", { courses: multipleMongooseToObject(courses) })
+      )
+      .catch(next);
   }
 
   search(req, res) {
