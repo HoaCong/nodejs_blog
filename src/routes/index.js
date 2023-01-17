2abad93e630d92247ec9bc2ab5cdd63fa387dcfd
Dev_HoaLe <leconghoa11a1@gmail.com>
2023-01-17 15:49:52 +0700

    update course detail

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const newRouter = require("./news");
+const coursesRouter = require("./courses");
 const siteRouter = require("./site");
 function route(app) {
   app.use("/news", newRouter);
+  app.use("/courses", coursesRouter);
 
   app.use("/", siteRouter);
 }
