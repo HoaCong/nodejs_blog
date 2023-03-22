3d503b3430c52fc81f036790b55cca4a6ce6e128
Dev_HoaLe <leconghoa11a1@gmail.com>
2023-03-22 17:29:45 +0700

    create new course

---
+++ README.md
@@ -0,0 +1,3 @@
+# yarn start chạy chương trình
+
+# yarn watch bắt các thay đổi CSS
--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -9,6 +9,19 @@ class CourseController {
       })
       .catch(next);
   }
+  // [GET] /courses/create
+  create(req, res, next) {
+    res.render("course/create");
+  }
+  // [POST] /courses/store
+  store(req, res, next) {
+    req.body.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
+    const course = new Course(req.body);
+    course
+      .save()
+      .then(() => res.redirect("/"))
+      .catch((err) => {});
+  }
 }
 
 module.exports = new CourseController();
