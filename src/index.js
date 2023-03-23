5a82f0e6bae4b5e3d7c12ebf5791b765e8a149ad
Dev_HoaLe <leconghoa11a1@gmail.com>
2023-03-23 17:30:53 +0700

    update course

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const path = require("path");
 const handlebars = require("express-handlebars");
+const methodOverride = require("method-override");
 
 const route = require("./routes");
 const db = require("./config/db");
@@ -15,14 +16,19 @@ const port = 3000;
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(methodOverride("_method"));
 // // HTTP logger
 // app.use(morgan("combined"));
 
 // template engine
 app.engine(
   "hbs",
-  handlebars.engine({ defaultLayout: "main", extname: ".hbs" })
+  handlebars.engine({
+    extname: ".hbs",
+    helpers: {
+      sum: (a, b) => a + b,
+    },
+  })
 );
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources", "views"));
